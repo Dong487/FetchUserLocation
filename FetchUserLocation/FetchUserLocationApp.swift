9b540fba56f304b1cea0b99fa35ccc2b322c8f06
Dong487 <Dsheng7777777@gmail.com>
2022-08-11 13:27:17 +0800

    Initial Commit

---
+++ FetchUserLocation/FetchUserLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchUserLocationApp.swift
+//  FetchUserLocation
+//
+//  Created by DONG SHENG on 2022/8/11.
+//
+
+import SwiftUI
+
+@main
+struct FetchUserLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
